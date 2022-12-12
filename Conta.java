fc3582b80b8e82852ad9411a43589f7730e5c3f8
Leticia <92949025+leticiaesttrela@users.noreply.github.com>
2022-12-12 07:33:44 -0300

    Update Conta.java

--- Conta.java
+++ Conta.java
@@ -2,9 +2,42 @@ public abstract class Conta {
     private int numero;
     private String nome_titular;
     protected double saldo;
-public Conta(int numero, String nome_titular){
-    this.numero=numero;
-    this.nome_titular=nome_titular;
-    this.saldo=saldo;
-}
-}
+    public Conta() {}
+
+    public Conta(int numero, String nome_titular, String cpfTitular) {
+        this.numero = numero;
+        this.titular = new Cliente(nome_titular, cpfTitular);
+    }
+    
+    void depositar(double valor) {
+        this.saldo = this.getSaldo() + valor;
+    }
+    
+    public abstract boolean sacar(double valor);
+
+    public double getSaldo() { 
+        return this.saldo; 
+    }
+
+    public void setSaldo(Double saldo) { 
+        this.saldo = saldo; 
+    } 
+
+    public Cliente getNome_titular(){
+        return this.titular;
+    } 
+
+    public int getNumero(){
+        return this.numero;
+    }
+    public void setNumero(Integer numero) { 
+        this.numero = numero; 
+    } 
+    
+    public Cliente getTitular() {
+        return titular;
+    }
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+}   
