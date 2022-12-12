cbdd7013fa02710e67ad08aeaaf1d01235f19310
Leticia <92949025+leticiaesttrela@users.noreply.github.com>
2022-12-12 07:30:29 -0300

    Update ContaPoupança.java

--- ContaPoupança.java
+++ ContaPoupança.java
@@ -1,4 +1,14 @@
 public class ContaPoupança extends Conta {
+    public ContaPoupanca() {}
+
+    public ContaPoupanca(int numero, String nome_titular, String cpfTitular) {
+        super(numero, nome_titular, cpfTitular);
+    }
+    
+    public void rejustar(double percentual) {
+        double reajuste = this.getSaldo() * percentual;
+        this.depositar(reajuste);
+    }
     
     public boolean sacar(double valor){
         if(this.getSaldo() >= valor){
